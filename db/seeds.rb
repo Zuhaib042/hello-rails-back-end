1c612c6daefa4edabf25aabf8df8665c2d7c3e44
Zuhaib Amjad <umar0423@gmail.com>
2023-02-21 18:27:59 +0500

    Update rack-cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+messages = Message.create([
+  {greeting: 'Hello there'},
+  {greeting: 'Bonjour'},
+  {greeting: 'Ahlan bik, Marhaban'},
+  {greeting: 'Khush Aamdeed'},
+  {greeting: 'Gi Aayan Noon'},
+])
